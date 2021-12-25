8a5af074dd4dd797f4f85778d6748dd1b6fbf3ab
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2021-12-25 22:07:01 +0700

    Add Reducer and update Context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import UserResults from "../component/users/UserResults"
+import UserSearch from "../component/users/UserSearch"
 
 function Home() {
   return (
-    <div>
+    <>
+      <UserSearch />
       <UserResults />
-    </div>
+    </>
   )
 }
 
