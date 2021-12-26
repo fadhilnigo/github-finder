2803f3a495c856b087c9bed27b9d7ec3d67c6d7f
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2021-12-26 08:47:24 +0700

    Add Alert Component and User page

--- src/App.js
+++ src/App.js
@@ -4,27 +4,34 @@ import Footer from './component/layout/Footer'
 import About from './pages/About'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import User from './pages/User'
+import Alert from './component/layout/Alert'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
         <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          
-          <main className='container mx-auto px-3'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About/>} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          
-          <Footer />
-        </div>
-      </Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+        
+            <main className='container mx-auto px-3'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/user/:login' element={<User/>} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
